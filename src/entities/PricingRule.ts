6306e8c50a2f349cc20745d52943321969867e13
Abhinav Shrivastava <abhinav@learn.cafe>
2020-11-26 12:50:01 +0530

    Added: Discount and Total Items fields

--- src/entities/PricingRule.ts
+++ src/entities/PricingRule.ts
@@ -12,21 +12,26 @@ class PricingRule implements IPricingRule {
     public id: number;
     public code: string;
     public type: PricingType;
-    public description: string;
+    public description: string | undefined;
+    public discount: number;
+    public totalItems: number | undefined;
     public itemType: ItemType;
 
-    constructor(id: number, code: string, type: PricingType, description: string, itemType: ItemType) {
+    constructor(id: number, code: string, type: PricingType, discount: number, itemType: ItemType, description?: string, totalItems?: number) {
         this.id = id;
         this.code = code;
         this.type = type;
-        this.description = description;
         this.itemType = itemType;
+        this.discount = discount;
+        this.totalItems = totalItems;
+        this.description = description;
     }
 
     public applyRule(items: Item[]): number {
         let discountAmount = 0;
         for(const item of items) {
-            discountAmount += item.retailPrice * 0.50;
+            if (this.type === PricingType.flat)
+                discountAmount += item.retailPrice - this.discount;
         }
         return discountAmount;
     }
@@ -34,6 +39,7 @@ class PricingRule implements IPricingRule {
 
 export enum PricingType {
     flat = 'flat',
+    percent = 'percent',
     quantity = 'quantity'
 }
 
--- tests/unit/PricingRule.test.ts
+++ tests/unit/PricingRule.test.ts
@@ -3,7 +3,7 @@ import Item, {ItemType} from "../../src/entities/Item";
 
 describe('Tests Pricing Rules', () => {
     it('Should give a flat discount on small pizzas', (done) => {
-        const pricingRule: PricingRule = new PricingRule(1, 'SMALL', PricingType.flat, 'Flat discount on small pizzas', ItemType.small);
+        const pricingRule: PricingRule = new PricingRule(1, 'SMALL', PricingType.flat, 50, ItemType.small, 'Flat discount on small pizzas');
         const items: Item[] = [] as Item[];
         items.push(new Item(1, 'Small Pizza', 'Small Pizza', 100, ItemType.small));
         items.push(new Item(1, 'Small Pizza', 'Small Pizza', 100, ItemType.small));
